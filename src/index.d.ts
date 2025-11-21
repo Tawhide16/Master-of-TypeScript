206eccb5c6bfb6f5e76d04601bdb2f017a2a13f3
Tawhide16 <tawhideh.b10@gmail.com>
2025-11-21 11:59:33 +0600

    add Destructuring in rest

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,13 +1,2 @@
 export {};
-interface User {
-  name: {
-    first: string;
-    last: string;
-  };
-}
-
-function sendGifts(...friends: string[]) {
-  friends.forEach((friend) => {
-    console.log(`Sending gift to ${friend}`);
-  });
-}               
+    
