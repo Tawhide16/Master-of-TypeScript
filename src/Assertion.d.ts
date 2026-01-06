3aee773d635263e2ff18d7522b1371a4b5fe1fd6
Tawhide16 <tawhideh.b10@gmail.com>
2026-01-06 12:33:22 +0600

    add all

--- src/Assertion.d.ts
+++ src/Assertion.d.ts
@@ -1,2 +1,13 @@
 export {};
 //# sourceMappingURL=Assertion.d.ts.map
+// --- IGNORE ---
+declare function getusername(name: string | null): void;
+// --- IGNORE ---
+declare function getpassword(name:)
+declare function setpassword(name: string,)
+declare function getpassword(name:)
+string | null): void;
+declare function setpassword(name: string, password: string | null): void;
+// --- IGNORE ---
+
+
